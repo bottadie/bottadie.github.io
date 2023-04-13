c2fa9c9b6a520dc4be5c532716895f664b710bb0
bottadie <92654346+bottadie@users.noreply.github.com>
2023-04-13 12:09:35 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/QR-Generator-FEM/"
+  base: "/bottadie.github.io/"
+  
 })
